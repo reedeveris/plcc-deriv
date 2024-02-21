a1739d119e771ef2a14d24c4c2683401bfbe7cea
Reed Everis <reednorrish@gmail.com>
2024-02-21 20:11:10 +0000

    continue refactoring scan.py

--- src/Std/PythonS/Scan.py
+++ src/Std/PythonS/Scan.py
@@ -10,13 +10,13 @@ class Scan(IScan):
     end = None              # ending position
     lno = None              # current line number
     tok = None              # self is persistent across all calls to cur()
-    lineMode = None         # token to toggle line mode
+    self.lineMode = None         # token to toggle line mode
 
     # create a scanner object on a buffered reader
     def __init__(self, rdr):
         self.rdr = rdr
         self.lno = 0
-        self.lineMode = None
+        self.self.lineMode = None
         self.s = None
         self.tok = None
         # force the enum Match class to compile its patterns
@@ -30,14 +30,14 @@ class Scan(IScan):
    def __init__(self, s):
         self(io.BufferedReader(io.StringIO(s)))
 
-    def reset():
+    def reset(self):
         # force the scanner to process the next line
         self.s = None
         self.tok = None
-        self.lineMode = None
+        self.self.lineMode = None
 
     # fill the string buffer from the reader if it's exhausted or None)
-    def fillString():
+    def fillString(self):
         if s == None or start >= end:
             # get the next line from the reader
             try:
@@ -52,38 +52,39 @@ class Scan(IScan):
                 s = None
             # System.err.print("s=" + s);
 
-    def cur():
+    def cur(self):
         # lazy
-        if tok != None:
-            return tok # don't get a new token if we already have one
+        if self.tok != None:
+            return self.tok # don't get a new token if we already have one
 
         matchString = ""
         matchFound = None
 
+        LOOP:
         while true :
             fillString() # get another line if necessary
             if self.s == None:
-                tok = Token(Token.eof, "!EOF", lno, None) # EOF
-                return tok
+                self.tok = Token(Token.eof, "!EOF", lno, None) # EOF
+                return self.tok
             # s cannot be None here
             # are we in line mode?
-            if lineMode != None:
-                cpat = lineMode.match.cPattern
+            if self.lineMode != None:
+                cpat = self.lineMode.match.cPattern
                 m = re.match(cpat, s)
                 m.pos = 0
                 m.endpos = end
                 start = end # consume the line before next match
                 if m:
-                    # found the lineMode token, exit line mode
-                    # and return the matched lineMode token
+                    # found the self.lineMode token, exit line mode
+                    # and return the matched self.lineMode token
                     # System.out.println("leaving line mode...");
-                    tok = Token(lineMode.match, m.group(), lno, s)
-                    lineMode = None
-                    return tok
+                    self.tok = Token(self.lineMode.match, m.group(), lno, s)
+                    self.lineMode = None
+                    return self.tok
                 else:
                     # return the entire line as a token
-                    tok = Token(Token.Match.LINE, s, lno, s)
-                    return tok
+                    self.tok = Token(Token.Match.LINE, s, lno, s)
+                    return self.tok
 
             matchEnd = start # current end of match
             for match in Token.Match.values():
@@ -92,110 +93,91 @@ class Scan(IScan):
                     break # nothing matches, so can't find a token
                 if match.tokType == Token.TokType.SKIP and matchFound != None:
                     continue # ignore skips if we have a pending token
-                if start != 0 and match.pattern.[0] == '^':
+                if start != 0 and match.pattern[0] == '^':
                     continue # '^' must match at start of line
                 m = re.match(cpat, s)
                 m.pos = 0
                 m.endpos = end
                 if m:
                     e = m.endpos
-                    if (e == start)
-                        continue; # empty match, so try next pattern
-                    if (match.tokType == Token.TokType.SKIP) {
+                    if e == start:
+                        continue # empty match, so try next pattern
+                    if match.tokType == Token.TokType.SKIP:
                         # there's a non-empty skip match,
                         # so we skip over the matched part
                         # and get more stuff to read
-                        start = e;
-                        continue LOOP;
-                    }
-                    if (matchEnd < e) {
+                        start = e
+                        continue LOOP
+                    if matchEnd < e:
                         # found a longer match -- keep it!
-                        matchEnd = e;
-                        matchString = m.group();
-                        matchFound = match;
-                    }
-                }
-            }
-            if (matchFound == None) { # got to $ERROR, so nothing matches!!
-                char ch = s.charAt(start++); # grab the char and advance
-                String sch;
-                if (ch >= ' ' && ch <= '~')
-                    sch = String.format("\"%c\"", ch);
+                        matchEnd = e
+                        matchString = m.group()
+                        matchFound = match           
+            if matchFound == None: # got to $ERROR, so nothing matches!!
+                ch = s[start+1] # grab the char and advance
+                sch = None
+                if ch >= ' ' and ch <= '~':
+                    sch = '"{}"'.format(ch)
                 else
-                    sch = String.format("\\u%04x", (int)ch);
-                tok = new Token(Token.Match.$ERROR, "!ERROR("+sch+")", lno, s);
-                return tok;
-            }
-            start = matchEnd; # start of next token match
+                    sch = '%04x'.format((int)ch)
+                self.tok = Token(Token.Match.ERROR, "!ERROR("+sch+")", lno, s)
+                return self.tok
+            start = matchEnd # start of next token match
             # matchString is the matching string
-            tok = new Token(matchFound, matchString, lno, s); # persistent
+            self.tok = Token(matchFound, matchString, lno, s) # persistent
             # System.out.println(String.format("match=%s\n", toggle));
-            if (matchFound.tokType == Token.TokType.LINE_TOGGLE) {
+            if matchFound.tokType == Token.TokType.LINE_TOGGLE:
                 # System.out.println("going to line mode...");
-                start = end; # swallow the rest of the line
-                lineMode = tok;
-            }
-            return tok;
-        }
-    }
+                start = end # swallow the rest of the line
+                self.lineMode = self.tok
+            return self.tok
 
-    public void adv() {
+    def adv(self):
         # if we have already advanced past the current token,
         # we'll have to do it again
-        if (tok == None)
-            cur();
-        tok = None;
-    }
+        if self.tok == None:
+            cur()
+        self.tok = None
 
-    public void put(Token t) {
-            throw new PLCCException("PLCC Scan error",
-                                    "put not implemented");
-    }
+    def put(self, t):
+        raise PLCCException("PLCC Scan error", "put not implemented")
 
     # See if the expected token match is the same as the match
     # of the current token
-    public Token match(Token.Match match, Trace trace) {
-        Token t = cur();
-        Token.Match mcur = t.match; # the token we got
-        if (match == mcur) { # compare the match expected with the token we got
-            if (trace != None)
-                trace.print(t);
-            adv();
-        } else {
-            String msg = "expected token " + match + ", got " + t.errString();
-            throw new PLCCException ("Parse error", msg);
-        }
-        return t;
-    }
+    def match(self, match, trace):
+        t = cur()
+        mcur = t.match # the token we got
+        if match == mcur: # compare the match expected with the token we got
+            if trace != None:
+                trace.print(t)
+            adv()
+        else:
+            msg = "expected token " + match + ", got " + t.errString()
+            raise PLCCException ("Parse error", msg)
+        return t
 
-    public boolean isEOF() {
-        return cur().isEOF();
-    }
+    def isEOF(self):
+        return cur().isEOF()
 
-    public void printTokens() {
-        while (hasNext()) {
-            Token t = next();
-            String s;
-            switch(t.match) {
-            case $ERROR:
-                s = t.toString();
-                break;
-            default:
-                s = String.format("%s '%s'", t.match.toString(), t.toString());
-            }
-            System.out.println(String.format("%4d: %s", lno, s));
-        }
-    }
+    def printTokens(self):
+        while self.hasNext():
+            t = next()
+            s = None
+            match t.match:
+                case ERROR:
+                    s = t.toString()
+                    break
+                case _:
+                    s = "{} '{}'".format(t.match.toString(), t.toString())
+            print("%4d: %s".format("%4d: %s", lno, s))
 
-    public boolean hasNext() {
-        return !cur().isEOF();
-    }
+    def hasNext():
+        return not cur().isEOF()
 
-    public Token next() {
-        Token t = cur();
-        adv();
-        return t;
-    }
+    def next():
+        t = cur()
+        adv()
+        return t
 
     public static void main(String [] args) {
         BufferedReader rdr = None;
@@ -215,5 +197,15 @@ class Scan(IScan):
         }
         Scan scn = new Scan(rdr);
         scn.printTokens();
-    }
-}
+
+if __name__ == __main__:
+    rdr = None
+    if len(sys.argv) == 0:
+        rdr = io.BufferedReader(io.StringIO(sys.stdin))
+    elif len(sys.argv) == 1:
+        try:
+            rdr = io.BufferedReader(new FileReader(args[0]));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
